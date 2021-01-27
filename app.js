8155c1678cde99b3af5f15035c370725a64f9a4e
edeced <ed_ced@icloud.com>
2021-01-27 00:52:32 -0600

    adds mini caching

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const API_URL = 'https://trackapi.nutritionix.com/v2/search' 
-const X_APP_ID = '2b73860b'
-const X_APP_KEY = '3021e4b8b20e1bd8b5bb19d38c129c6c'
+const X_APP_ID = '050e0a98'
+const X_APP_KEY = '3b310312d9b72ed6b3024c37f58305a3'
+
+/** alternative keys */
+// const X_APP_ID = '2b73860b'
+// const X_APP_KEY = '3021e4b8b20e1bd8b5bb19d38c129c6c'
 const previousSearch = []
 const DEBUG = false; //to see helpful notes, set to true
 
@@ -9,56 +13,92 @@ const DEBUG = false; //to see helpful notes, set to true
 
 const searchBtn = document.querySelector('#food-search');
 const input = document.querySelector('#food-query');
+const foodDetailsForm = document.querySelector('#food-details')
+
+const singleFormId = document.querySelector('#form-id')
+
+const cacheResults = JSON.parse(getFromStore("allFoods"))
+
+if(cacheResults.length !== 0 ){
+    updatePage(cacheResults)    
+}
+
+if(getFromStore('query') !== ""){
+    input.value = getFromStore('query')
+}
 
 // gets all food that matches
 async function getAllFood(query){
+
+    if(getFromStore('query') === query){
+   
+        return JSON.parse(getFromStore("allFoods"))
+   
+    } else {
+        const results = await fetch(`${API_URL}/instant?query=${query}`,{
+            headers : {
+                'x-app-id' : X_APP_ID ,
+                'x-app-key': X_APP_KEY
+            }
+        });
     
-    const results = await fetch(`${API_URL}/instant?query=${query}`,{
-        headers : {
-            'x-app-id' : X_APP_ID ,
-            'x-app-key': X_APP_KEY
-        }
-    });
+        const data = await results.json()
+    
+        if(DEBUG) console.log(data);
+    
+        return data.branded
+    }
 
-    const data = await results.json()
+   
 
-    if(DEBUG) console.log(data);
-
-    return data.branded
-
-}
-
-async function getSingleFood(itemId){
-    const results = await fetch(`${API_URL}/item?nix_item_id=${itemId}`,{
-        headers : {
-            'x-app-id' : X_APP_ID,
-            'x-app-key': X_APP_KEY
-        }
-    });
-
-    const data = await results.json()
-
-    return data.foods[0]
 }
 
 searchBtn.addEventListener('click', async () => {
 
-    addPreviousSearch(input.value)
-
-    const allFoods = await getAllFood(input.value);
+    const query = input.value
     
-    const singleFood = await getSingleFood(allFoods[0].nix_item_id)
+    const allFoods = await getAllFood(input.value);
+
+    store('allFoods', JSON.stringify(allFoods))
+    store('query', input.value)
 
     if(DEBUG){
         console.log(`Alls matching ${input.value}: `, allFoods);
-        console.log(`The Id of the ID of the first el: `, allFoods[0].nix_item_id);
-        console.log(`More details about the single el: `, singleFood)
+        // console.log(`The Id of the ID of the first el: `, allFoods[0].nix_item_id);
+        // console.log(`More details about the single el: `, singleFood)
     }
     
-    if(DEBUG) console.log([previousSearch])
+    if(DEBUG) console.log(previousSearch)
+
+    updatePage(allFoods)
 
 })
 
-function addPreviousSearch(item){
-    previousSearch.push(item)
+
+function updatePage(data = []){
+    const resultsDiv = document.querySelector('#results')
+
+    // clear the results sections first
+    resultsDiv.innerHTML = ''
+
+    data.forEach( item => {
+        const div = document.createElement('div')
+
+        div.innerHTML = `
+            <h3><a href='/details.html?id=${item.nix_item_id}'>${item.food_name} by ${item.brand_name}</a></h3>
+            <img src="${item.photo.thumb}" width="150px" height="150px" alt="${item.food_name}">
+            <h4>Calories: ${item.nf_calories}</h4>
+        
+            <hr>
+        `
+        resultsDiv.appendChild(div)
+    })
+}
+
+function store(itemName, itemValue){
+    window.localStorage.setItem(itemName, itemValue)
+}
+
+function getFromStore(item){
+    return window.localStorage.getItem(item)
 }
---
+++ details.js
@@ -0,0 +1,37 @@
+const API_URL = 'https://trackapi.nutritionix.com/v2/search' 
+const X_APP_ID = '050e0a98'
+const X_APP_KEY = '3b310312d9b72ed6b3024c37f58305a3'
+
+//1. Get the ID via the queryString from the current URL
+const queryString = window.location.search
+const params = new URLSearchParams(queryString); // create a new object
+const id = params.get('id')
+// method chaining
+getSingleFood(id)
+    .then(item =>updatePage(item))
+    // .then(item =>updatePage(item))
+
+
+function updatePage(item = {}){
+    document.querySelector('#details')
+        .innerHTML = `
+            <h3>${item.food_name} by ${item.brand_name}</h3>
+            <img src="${item.photo.thumb}" alt="${item.food_name}">
+            <p>${item.nf_ingredient_statement}</p>
+        `
+}
+//2. Use the ID to query the API
+async function getSingleFood(itemId){
+    const results = await fetch(`${API_URL}/item?nix_item_id=${itemId}`,{
+        headers : {
+            'x-app-id' : X_APP_ID,
+            'x-app-key': X_APP_KEY
+        }
+    });
+
+    const data = await results.json()
+
+    return data.foods[0]
+}
+
+//3. update the page with data...
